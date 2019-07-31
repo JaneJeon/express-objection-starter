262bd86acd0ea9367de55ea38735d70c7c501c0f
Jane Jeon <JaneJeon9719@gmail.com>
2019-07-31 19:47:12 -0400

    email config

--- services/email.js
+++ services/email.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer")
 const SES = require("aws-sdk/clients/ses")
+const logger = require("./logger")
 
-module.exports = nodemailer.createTransport({
-  SES: new SES()
-})
+module.exports = nodemailer.createTransport(
+  { SES: new SES(), logger },
+  { from: process.env.EMAIL_ADDRESS }
+)
