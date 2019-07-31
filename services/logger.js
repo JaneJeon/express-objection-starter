e26bd6b5a03639e76668c70f41a0bf0cd7482ffd
Jane Jeon <JaneJeon9719@gmail.com>
2019-07-31 02:15:31 -0400

    added exit logging

--- services/logger.js
+++ services/logger.js
@@ -14,4 +14,20 @@ const logger = (() => {
   }
 })()
 
+process.on(
+  "uncaughtException",
+  pino.final(logger, (err, finalLogger) => {
+    finalLogger.error(err, "uncaughtException")
+    process.exit(1)
+  })
+)
+
+process.on(
+  "unhandledRejection",
+  pino.final(logger, (err, finalLogger) => {
+    finalLogger.error(err, "unhandledRejection")
+    process.exit(1)
+  })
+)
+
 module.exports = logger
