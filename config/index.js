a21fc4d24884209ef582b3c8ce94f1ba1dafee03
Jane Jeon <JaneJeon9719@gmail.com>
2019-08-07 16:30:48 -0400

    move config files into a folder a la rails

--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@ nconf
   .argv({ parseValues: true })
   .env({ parseValues: true, lowerCase: true, separator: '_' })
 
-if (nconf.get('node:env')) nconf.file(`./config.${nconf.get('node:env')}.json`)
+if (nconf.get('node:env'))
+  nconf.file(`./environments/${nconf.get('node:env')}.json`)
 
-module.exports = nconf.defaults(require('./config.default'))
+module.exports = nconf.defaults(require('./environments/default'))
