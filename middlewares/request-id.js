e64801dfc3a0c09a642b42e5e848c6510952c48f
Jane Jeon <JaneJeon9719@gmail.com>
2019-08-07 14:56:36 -0400

    cache request id header value

--- middlewares/request-id.js
+++ middlewares/request-id.js
@@ -1,12 +1,10 @@
 let nextReqId = 0
 const maxInt = 2147483647
 
-const config = require('../config')
+const reqIdHeader = require('../config').get('requestIdHeader')
 
 // https://github.com/fastify/fastify/blob/master/lib/reqIdGenFactory.js
 module.exports = (req, res, next) => {
-  req.id =
-    req.header(config.get('requestIdHeader')) ||
-    (nextReqId = (nextReqId + 1) & maxInt)
+  req.id = req.header(reqIdHeader) || (nextReqId = (nextReqId + 1) & maxInt)
   next()
 }
