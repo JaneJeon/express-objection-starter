a881bcb0f6ffb646a084384e9aeeb438231dc4e4
Jane Jeon <JaneJeon9719@gmail.com>
2019-08-31 04:05:46 -0400

    update file associations

--- scripts/generate-templates.js
+++ scripts/generate-templates.js
@@ -6,6 +6,7 @@ const mjml2html = require('mjml')
 // see https://handlebarsjs.com/precompilation.html for more details
 glob.sync('views/emails/**/html.mjml').forEach(mjmlFile => {
   fs.readFile(mjmlFile, (err, mjml) => {
+    // prettier doesn't support hbs, so we need to manually format it
     const { html } = mjml2html(mjml.toString(), { beautify: true })
     const htmlFile = path.join(path.dirname(mjmlFile), 'html.hbs')
     fs.writeFile(htmlFile, html, err =>
