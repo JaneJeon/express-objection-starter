c4b7000e868e5e24d6a4322a49d2b36383446387
Jane Jeon <JaneJeon9719@gmail.com>
2019-10-12 21:31:19 -0400

    lint

--- scripts/generate-templates.js
+++ scripts/generate-templates.js
@@ -10,6 +10,7 @@ glob.sync('views/emails/**/html.mjml').forEach(mjmlFile => {
     const { html } = mjml2html(mjml.toString())
     const htmlFile = path.join(path.dirname(mjmlFile), 'html.hbs')
     fs.writeFile(htmlFile, html, () =>
+      // eslint-disable-next-line no-console
       console.log(`${mjmlFile} => ${htmlFile}`)
     )
   })
