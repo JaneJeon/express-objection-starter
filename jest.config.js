2e0926ab758aa2062d0e7cd6427b6d8b4ac1dd68
Jane Jeon <JaneJeon9719@gmail.com>
2020-01-08 02:53:42 -0500

    add github actions

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,7 @@
-const isCI = require('is-ci')
-
-process.env.JEST_JUNIT_OUTPUT_NAME = `reports/junit/jest/node-${process.version}results.xml`
+process.env.JEST_JUNIT_OUTPUT_NAME = `reports/junit/jest/node-${process.version}/db-${process.env.KNEX_CLIENT}-results.xml`
 
 module.exports = {
-  collectCoverage: isCI,
   coveragePathIgnorePatterns: ['node_modules', 'views/emails/index.js'],
   errorOnDeprecated: true,
-  reporters: isCI ? ['default', 'jest-junit'] : undefined,
   testEnvironment: 'node'
 }
